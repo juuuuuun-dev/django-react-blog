a30491ff4fceda18424046ef235bc719ae5dc67a
junkata <junkata.dev@gmail.com>
2020-03-04 10:34:18 +0900

    add gitignore

--- backend/django/config/urls.py
+++ backend/django/config/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url, static
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
