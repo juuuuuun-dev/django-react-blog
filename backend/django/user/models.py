cc647161cf337803d50d334a2727ce6603e34625
junkata <junkata.dev@gmail.com>
2020-03-05 18:44:02 +0900

    add user

---
+++ backend/django/user/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(self, email, username, password=None):
+        if not email:
+            # throw
+            raise ValueError('email is required')
+        user = self.model(email=self.normalize_email(email), username=username)
+        user.setpassword(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    class Meta:
+        db_table = 'users'
+
+    username = models.CharField(verbose_name='username', max_length=254, unique=True)
+    email = models.EmailField(verbose_name='email', max_length=254)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ("username",)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avator = models.FileField(upload_to="media/avatars/", null=True, blank=True)
+    message = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, created = UserProfile.objects.get_or_create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=User)
