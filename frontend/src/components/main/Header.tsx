1750a4bb18b15e363c4fb6ba4d922ade6a917d35
junkata <junkata.dev@gmail.com>
2020-07-09 15:30:41 +0900

    add init data

--- frontend/src/components/main/Header.tsx
+++ frontend/src/components/main/Header.tsx
@@ -1,10 +1,15 @@
+import { Col, Drawer, Layout, Row } from 'antd';
 import React from 'react';
-import { Layout, Drawer, Row, Col } from 'antd';
-import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+import { CloseOutlined, MenuOutlined } from '@ant-design/icons';
 import { useWindowSize } from '@react-hook/window-size';
+
+import { MainContext } from '../../context/mainContext';
 import Nav from './Nav';
 
 const Header: React.FC = () => {
+  const [{ init, appTitle }] = React.useContext(MainContext);
   const headerHeight = '60px';
   const { Header } = Layout;
   const [showDrawer, setShowDrawer] = React.useState<boolean>(false);
@@ -18,19 +23,17 @@ const Header: React.FC = () => {
           height: headerHeight,
           background: '#ffffff',
           boxShadow: "0 4px 12px 0 rgba(0, 0, 0, 0.05)",
-          // position: 'fixed',
-          // top: 0,
-          // left: 0,
-          // width: '100%'
+
         }}
       >
         <div className="header-container">
           <Row>
-            <Col flex="100px">Logo</Col>
+            <Col flex="100px"><Link to="/"><div>{appTitle}</div></Link></Col>
             <Col flex="auto">
               {width > 600 && (
                 <Nav
                   mode="horizontal"
+                  categories={init?.categories}
                   styles={{
                     lineHeight: headerHeight,
                     float: 'right',
@@ -40,21 +43,20 @@ const Header: React.FC = () => {
               {width <= 600 && <Humberger showDrawer={showDrawer} setShowDrawer={setShowDrawer} />}
             </Col>
           </Row>
-          <div className="logo"></div>
         </div>
       </Header>
 
       <Drawer
-        title="Basic Drawer"
         placement="right"
         closable={false}
+        className="nav-drawer"
         onClose={() => setShowDrawer(false)}
         visible={showDrawer}
         style={{
           top: headerHeight,
         }}
       >
-        <Nav mode="vertical" handleClick={navOnClick} styles={{}} />
+        <Nav mode="inline" categories={init?.categories} setShowDrawer={setShowDrawer} styles={{}} />
       </Drawer>
     </>
   );
@@ -66,9 +68,9 @@ export interface HumbergerProps {
 }
 const Humberger = ({ showDrawer, setShowDrawer }: HumbergerProps) => {
   return (
-    <div style={{ float: 'right' }}>
-      {!showDrawer && <MenuOutlined onClick={() => setShowDrawer(true)} />}
-      {showDrawer && <CloseOutlined onClick={() => setShowDrawer(false)} />}
+    <div className="humberger">
+      {!showDrawer && <div className="humberger-button" onClick={() => setShowDrawer(true)}><MenuOutlined className="humberger-button__icon" /></div>}
+      {showDrawer && <div className="humberger-button" onClick={() => setShowDrawer(false)}><CloseOutlined className="humberger-button__icon" /></div>}
     </div>
   );
 };
