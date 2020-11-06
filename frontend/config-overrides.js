413d71df775527a0ba120cc370f3c4c0bfb32406
junkata <junkata.dev@gmail.com>
2020-11-07 07:58:20 +0900

    delete line

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -6,19 +6,6 @@ const {
 } = require("customize-cra");
 const CopyPlugin = require('copy-webpack-plugin');
 
-
-// const plugin = new CopyPlugin({
-//   patterns: [{
-//     from: 'public/manifest.json',
-//     transform(content, path) {
-//       content = content.toString()
-//         .replace(/process.env.API_BASE_URL/g, process.env.API_BASE_URL)
-//         .replace(/process.env.REACT_APP_TITLE/g, process.env.REACT_APP_TITLE)
-//         .replace(/process.env.REACT_APP_THEME_COLOR/g, process.env.REACT_APP_THEME_COLOR)
-//       return content;
-//     },
-//   }],
-// });
 module.exports = override(
   fixBabelImports("import", {
     libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
